9c564b370280833d2bdedc4af40d10cd856410c0
priyank96 <priyankbhandia@hotmail.com>
2017-02-22 23:20:33 +0530

    Dummy Pushback

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.Globalization;
+namespace StatesAndGrammars
+{
+
+    public class Grammars
+    {
+        static SpeechRecognitionEngine Pushback(SpeechRecognitionEngine sre, String atcName, String callSign)
+        {
+
+            //request for pushback
+
+
+            Choices request = new Choices(new string[] { "request", "requesting", "ready" });
+            GrammarBuilder gb_request = new GrammarBuilder();
+            gb_request.Append(atcName, 0, 1);
+            gb_request.Append(callSign);
+            gb_request.Append(request);
+            gb_request.Append("push back");
+
+            Grammar g_request = new Grammar(gb_request);
+            sre.LoadGrammarAsync(g_request);
+
+            //pushback readback
+            Choices answer = new Choices(new string[] { "roger", "push back approved facing south", "facing south pushback approved" });
+            GrammarBuilder gb_readback = new GrammarBuilder();
+            gb_readback.Append(answer);
+            gb_readback.Append(callSign);
+            //gb_readback.Append(atc, 0, 1);
+
+            Grammar g_readback = new Grammar(gb_readback);
+            sre.LoadGrammar(g_readback);
+
+            return sre;
+
+        }//set_grammars
+
+
+        public static SpeechRecognitionEngine getGrammars(SpeechRecognitionEngine sre, State state)
+        {
+            if (state.stateName == "pushback")
+            {
+                return Pushback(sre, state.GetAtcName(), state.GetCallSign());
+            }
+            else
+            {
+                return Pushback(sre, state.GetAtcName(), state.GetCallSign()); //Change when adding new states!!
+            }
+
+        }
+    }
+    public class State
+    {
+        //for a state which begins with ATC contact, speak first command from constructor, validate readback is first stateReply
+        public readonly String stateName;
+        private bool completed = false;
+        private int stateCount = 0;
+        private int readbackCount = 0;
+        private String[] stateReplies;
+       
+        private String[][] readbackInfo;
+        private string callSign;
+        private string atcName;
+        public bool IsCompleted()
+        {
+            return completed;
+        }
+        private void UpdateCompletion()
+        {
+            if (stateCount >= stateReplies.Length)
+            {
+                completed = true;
+            }
+            //Console.WriteLine(completed);
+        }
+
+        public String GetReply(String pilotTxt)
+        {
+            if (IsCompleted()) { return ""; }
+            String reply = "";
+            if (stateCount < stateReplies.Length)
+            {
+
+                if (stateReplies[stateCount] == "validate readback")
+                {
+                    pilotTxt = pilotTxt.ToLower();
+                    Console.WriteLine("validating..");
+                    String[] readback = readbackInfo[readbackCount];
+                    bool validated = false;
+                    foreach (String value in readback)
+                    {
+                        ///Console.Write(value);
+                        if (pilotTxt.Contains(value))
+                        {
+
+                            validated = true;
+                            Console.WriteLine("validated!");
+                            stateCount++;
+                            readbackCount++;
+                            reply = "";
+                            break;
+                        }
+
+                    }
+                    if (!validated)
+                    {
+                        reply = callSign + " say again";
+                    }
+                }
+                else
+                {
+                    reply = callSign + " " + stateReplies[stateCount++];
+                }
+
+            }
+            UpdateCompletion();
+            return reply;
+        }
+        public String GetCallSign()
+        {
+            return callSign;
+        }
+        public String GetAtcName()
+        {
+            return atcName;
+        }
+
+
+
+        public State(String StateName, String[] stateReplies, String[][] readbackInfo, String callSign, String atcName)
+        {
+
+            this.stateName = StateName;
+            this.stateReplies = stateReplies;
+            this.readbackInfo = readbackInfo;
+            this.callSign = callSign;
+            this.atcName = atcName;
+
+        }
+
+
+
+    }
+
+}
---
+++ ConsoleSpeech2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Speech.Recognition;
+using Microsoft.Speech.Synthesis;
+using System.Globalization;
+using StatesAndGrammars;
+namespace ConsoleSpeech2
+{
+    
+    class Program
+    {   
+            // ss,sre and state object made available everywhere
+            static SpeechSynthesizer ss = new SpeechSynthesizer();
+            static SpeechRecognitionEngine sre;
+            static State pushback;
+        
+        static void Main(string[] args)
+        {
+            String[] stateReplies = new String[]{"Pushback approved, facing south","validate readback"};
+            String[][] readbackInfo = { new String[] { "roger", "pushback approved facing south", "facing south pushback approved" } };
+            String callSign = "delta alpha tango one seven seven three";
+            String atcName = "apron";
+            String stateName = "pushback";
+            pushback = new State(stateName,stateReplies,readbackInfo, callSign, atcName);   
+        
+
+            //ss.Rate = -2; //arbitrary
+            ss.SetOutputToDefaultAudioDevice();
+            CultureInfo ci = new CultureInfo("en-us");
+            sre = new SpeechRecognitionEngine(ci);
+            sre.SetInputToDefaultAudioDevice();
+            sre.SpeechRecognized += sre_SpeechRecognized;            
+            sre = Grammars.getGrammars(sre, pushback);
+            Console.WriteLine("\nReady");
+            ss.Speak("Ready");
+            sre.RecognizeAsync(RecognizeMode.Multiple);
+            while (pushback.IsCompleted() == false) { ; }
+            Console.WriteLine("Pushback Completed!");
+            Console.Read();
+            
+            
+        }
+        
+
+        static void sre_SpeechRecognized(object sender,
+      SpeechRecognizedEventArgs e)
+        {
+            string txt = e.Result.Text;
+            float confidence = e.Result.Confidence;
+            Console.WriteLine("\nRecognized: " + txt);
+            if (confidence < 0.40) return; // arbitrary constant
+            //begin handling
+            String reply = pushback.GetReply(txt);
+            //Console.WriteLine( txt.Length);
+            if (txt.Length > 0) {
+                Console.WriteLine("\n<Saying>" + reply);
+                ss.Speak(reply);
+            }
+            
+        } // sre_SpeechRecognized
+  
+    }
+}
